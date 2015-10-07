494048859d536ea763c24abc19cffda0100e6931
Linzertorte <Linzertorte@users.noreply.github.com>
2015-10-06 21:23:50 -0700

    Update rubik.py

--- Rubik/rubik.py
+++ Rubik/rubik.py
@@ -1,4 +1,12 @@
 from graphics import *
+import random
+
+
+# change seq visulize your rotations
+# press space to watch next rotation
+seq = [1,2,-1,2,-3,5,-5,6]
+
+
 
 def up_face(x,y,k):
   return [[[Point(x+2*j*k-i*k,y+i*k),
@@ -40,6 +48,7 @@ board = [["YYY",
          ["RRR",
           "RRR",
           "RRR"]]
+
 rot1 = [[(1,0,0),(1,0,1),(1,0,2),(1,1,2),(1,2,2),(1,2,1),(1,2,0),(1,1,0)],
        [(2,0,0),(2,0,1),(2,0,2),(2,1,2),(2,2,2),(2,2,1),(2,2,0),(2,1,0)],
        [(3,0,0),(3,0,1),(3,0,2),(3,1,2),(3,2,2),(3,2,1),(3,2,0),(3,1,0)],
@@ -74,27 +83,41 @@ def rotate(x):
     f,i,j = rot2[x][k]
     board[f-1][i][j] = b2[k]
 
+colors = {'Y':'yellow',
+          'B':'blue',
+          'G':'green',
+          'M':'orange',
+          'R':'red',
+          'W':'pink'}
+
+board = [[list(line) for line in f] for f in board]
+
+def draw_rubik(win):
+  global faces, board, colors
+  for f in xrange(6):
+    for i in xrange(3):
+      for j in xrange(3):
+        vs = faces[f][i][j]
+        poly = Polygon(vs)
+        poly.setFill(colors[board[f][i][j]])
+        poly.setWidth(2)
+        poly.draw(win)
+
 def main():
-    win = GraphWin('Face', 400,400) # give title and dimensions
-    colors = {'Y':'yellow',
-              'B':'blue',
-              'G':'green',
-              'M':'orange',
-              'R':'red',
-              'W':'pink'} 
-    global faces,board
-    board = [[list(line) for line in f] for f in board]
-    for x in [5,-1,2,1,-1,-2,1,-5,1]:
-      for f in xrange(6):
-        for i in xrange(3):
-          for j in xrange(3):
-            vs = faces[f][i][j]
-            poly = Polygon(vs)
-            poly.setFill(colors[board[f][i][j]])
-            poly.setWidth(2)
-            poly.draw(win)
-      win.getKey()
+    global seq
+    win = GraphWin('Rubik', 400,400) # give title and dimensions
+    #seq = [random.randint(-5,6) for i in xrange(15)]
+    #seq = map(lambda x: x-1 if x<=0 else x,seq)
+    #print seq
+    draw_rubik(win)
+    win.getKey()
+    for x in seq:
       rotate(x)
+      draw_rubik(win)
+      label = Text(Point(100, 120), str(x))
+      label.draw(win)
+      win.getKey()
+      label.undraw()
       win.flush
     win.getMouse()
     win.close()
