38ca2769b190e6d950408cf9d566086d93bc4c99
Linzertorte <Linzertorte@users.noreply.github.com>
2018-08-10 16:44:41 -0700

    Update xima.js

--- xima.js
+++ xima.js
@@ -11,28 +11,31 @@ var download = function(url, dest, cb) {
   });
 }
 
-function down_album(album_id,page) {
-  var url = 'http://www.ximalaya.com/revision/play/album?sort=-1&pageSize=30'+'&albumId='+album_id+'&pageNum='+page
+function download_album(album_id,page) {
+  var url = 'http://www.ximalaya.com/revision/play/album?sort=-1&pageSize=30'+
+  '&albumId=' + album_id + '&pageNum=' + page
   http.get(url, function(res){
     var body = '';
     res.on('data', function(chunk){
       body += chunk;
     });
-
     res.on('end', function(){
       album = JSON.parse(body);
       var i = 0;
-      for(i = 0 ;i < album.data.tracksAudioPlay.length;i++) {
+      for(i = 0 ;i < album.data.tracksAudioPlay.length;i ++) {
         console.log(album.data.tracksAudioPlay[i].trackName)
-        download(album.data.tracksAudioPlay[i].src,album.data.tracksAudioPlay[i].trackName+".m4a",function() { console.log("done!")});
+        download(album.data.tracksAudioPlay[i].src,
+          album.data.tracksAudioPlay[i].trackName+".m4a",
+          function() { console.log("done!");}
+        );
       };
-      if(album.data.hasMore) down_album(album_id,page+1)
+      if(album.data.hasMore) download_album(album_id,page+1)
     });
   }).on('error', function(e){
     console.log("Got an error: ", e);
   });
 }
 
-//down_album(12960645,1)
+// download_album(12960645,1)
 // Example: nodejs xima.js 12960645
-down_album(process.argv[2],1)
+download_album(process.argv[2],1)
